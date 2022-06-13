629a551b0b01c23d6211a9030d4b6bda12dacdd6
Kazeem Quadri <quadrikazeem01@gmail.com>
2022-06-13 18:17:11 +0100

    Header section and project initialization with project files

---
+++ components/Header.tsx
@@ -0,0 +1,21 @@
+import { MenuIcon } from '@heroicons/react/solid';
+function Header() {
+  return (
+    <div className='grid grid-cols-2 px-3 py-4'>
+      <div className='cursor-pointer'>
+        <h1>Digital Agency</h1>
+      </div>
+      <div className='hidden md:flex grow justify-end cursor-pointer space-x-8'>
+        <a href='#'>Home</a>
+        <a href='#'>About</a>
+        <a href='#'>Testimonials</a>
+        <a href='#'>Contact</a>
+      </div>
+      <div className='md:hidden grow flex justify-end cursor-pointer'>
+        <MenuIcon className='h-5' />
+      </div>
+    </div>
+  );
+}
+
+export default Header;

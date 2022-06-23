590569cc09102c11e3670a12555fd8c148ebcdc7
Kazeem Quadri <quadrikazeem01@gmail.com>
2022-06-23 21:47:10 +0100

    Hero bug fix

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -3,16 +3,19 @@ import Button from './Button';
 
 function Hero() {
   return (
-    <div className='flex flex-col md:flex-row py-6'>
-      <div className='max-w-sm md:max-w-md/2 mx-auto space-y-6 py-8'>
-        <h1 className='font-bold text-5xl text-center md:text-left'>
-          Building digital products, brands & experience
-        </h1>
-        <p className='text-center md:text-left'>
-          Digital Agency is your online team mangement tool that easy and prompt
-        </p>
-        <div className='flex justify-center md:justify-start'>
-          <Button title='Contact Us' />
+    <div className='flex flex-col md:flex-row py-6 md:justify-around'>
+      <div className='max-w-sm md:max-w-sm mx-auto md:mx-2  py-8 '>
+        <div className='space-y-6'>
+          <h1 className='font-bold text-5xl text-center md:text-left'>
+            Building digital products, brands & experience
+          </h1>
+          <p className='text-center md:text-left'>
+            Digital Agency is your online team mangement tool that easy and
+            prompt
+          </p>
+          <div className='flex justify-center md:justify-start'>
+            <Button title='Contact Us' />
+          </div>
         </div>
       </div>
       <div className='flex justify-center md:max-w-lg'>
